66fd53c4a8651170e85860c3353bb7185c0d3dbb
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-05-14 16:03:42 -0600

    Se realizan cambios de diseño al componente 'CardPost'

--- src/components/Posts/CardPost.jsx
+++ src/components/Posts/CardPost.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react'
+
 export const CardPost = ({ title, category, content, author }) => {
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded)
+    }
+
     return (
-        <div className="max-w-sm w-full bg-white shadow-lg rounded-lg overflow-hidden">
-            <div className="p-6">
+        <div className="max-w-sm w-full bg-white shadow-lg rounded-lg overflow-hidden p-4">
+            <div className="p-4">
                 <h5 className="text-xl font-semibold mb-2">{title}</h5>
                 <h6 className="text-lg text-gray-600 mb-2">{author}</h6>
-                <p className="text-sm text-gray-800 mb-4">{content}</p>
+                <p className={`text-sm text-gray-800 mb-4 ${isExpanded ? '' : 'truncate'}`}>
+                    {content}
+                </p>
+                {content.length > 100 && (
+                    <button 
+                        onClick={toggleExpand} 
+                        className="text-blue-500 hover:underline"
+                    >
+                        {isExpanded ? 'Ver menos' : 'Ver más'}
+                    </button>
+                )}
                 <p className="text-sm text-gray-500">{category}</p>
                 <div className="mt-4 flex space-x-4">
                     <a href="#" className="text-blue-500 hover:underline">Card link</a>
@@ -12,5 +30,5 @@ export const CardPost = ({ title, category, content, author }) => {
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
