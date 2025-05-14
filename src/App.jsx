71427082a3fbe8acb1fda99fcd0d66581fcded6e
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-05-13 21:30:22 -0600

    Se envuelve 'elementRoutes' entre 'PostProvider'

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Toaster } from 'react-hot-toast'
 import { useRoutes } from 'react-router-dom'
 import { routes } from './routes'
+import { PostProvider } from './context/PostContext'
 
-const App = () => {
+function App () {
 
   const elementRoutes = useRoutes(routes);
 
   return (
     <>
-      {elementRoutes}
-      <Toaster  position='bottom-center' reverseOrder={false}/>
+      <PostProvider>
+        {elementRoutes}
+      </PostProvider>
+      <Toaster position='bottom-center' reverseOrder={false} />
     </>
   )
 }
