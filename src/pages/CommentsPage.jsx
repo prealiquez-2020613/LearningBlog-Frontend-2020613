b5222eaf414a020970f8e9858fe372784527aadd
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-05-14 19:26:00 -0600

    Se agrega el manejo de listar comentarios por publicación

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -1,19 +1,46 @@
 import { useParams } from 'react-router-dom'
-import { CommentForm } from '../components/comments/CommentForm'
+import { useEffect } from 'react'
 import { useApi } from '../shared/hooks/useApi'
+import { CommentForm } from '../components/comments/CommentForm'
+import { CardComment } from '../components/comments/CardComment'
 
 export const PostCommentsPage = () => {
   const { postId } = useParams()
-  const { addCommentary } = useApi()
+  const { addCommentary, getComments, comments, isFetchingComments } = useApi()
 
-  const handleAddComment = (commentData) => {
-    addCommentary(commentData)
+  useEffect(() => {
+    if (postId) {
+      getComments(postId)
+    }
+  }, [postId])
+
+  const handleAddComment = async (commentData) => {
+    await addCommentary(commentData)
+    getComments(postId)
   }
 
   return (
-    <div className="max-w-3xl mx-auto p-4">
+    <div className="max-w-3xl mx-auto p-4 space-y-6">
       <CommentForm postId={postId} onSubmit={handleAddComment} />
-      {/* Aquí más adelante el listado de comentarios */}
+
+      {isFetchingComments && <p>Cargando comentarios...</p>}
+
+      {!isFetchingComments && comments && comments.length === 0 && (
+        <p>No hay comentarios para esta publicación.</p>
+      )}
+
+      {!isFetchingComments && comments && comments.length > 0 && (
+        <div className="space-y-4">
+          {comments.map((comment) => (
+            <CardComment
+              key={comment._id}
+              author={comment.author}
+              content={comment.content}
+              createdAt={comment.createdAt}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
